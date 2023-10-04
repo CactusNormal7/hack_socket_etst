1214f593d547db172ab1256158ea14f5c93dbdf8
CactusNormal7 <jules.besson7@gmail.com>
2023-10-04 10:00:57 +0200

    d

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const http = require("http")
 const {Server} = require("socket.io")
 const cors = require("cors")
 
-
 app.use(cors())
 
 const server = http.createServer(app)
@@ -17,13 +16,19 @@ const io = new Server(server, {
 })
 
 io.on("connection", (socket) => {
+    let current_room = 0
     console.log(`User connected ${socket.id}`);
-
+    socket.on('room_number', (room) => {
+        socket.join(room.room)
+        current_room = room.room
+    })
+    
     socket.on('send_message', (data) => {
-        console.log(data.message);
-        socket.broadcast.emit("receive_message", data)
+        socket.broadcast.to(current_room).emit("receive_message", {data : data, sender : socket.id})
     })
 })      
+
+
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
     console.log("SERVER IS RUNNING");
