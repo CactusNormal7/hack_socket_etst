6a5daf7cce1976e95ae19e38e22a54a1008419bb
CactusNormal7 <jules.besson7@gmail.com>
2023-10-04 17:50:23 +0200

    d

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const http = require("http")
-const {Server} = require("socket.io")
+const { Server } = require("socket.io")
 const cors = require("cors")
 
 app.use(cors())
@@ -10,23 +10,41 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin : ["https://hackathon-test-iota.vercel.app", "http://localhost:3000"],
-        methods : ["GET", "POST"]
+        origin: ["https://hackathon-test-iota.vercel.app", "http://localhost:3000"],
+        methods: ["GET", "POST"]
     }
 })
 
+
+let users = {}
+
 io.on("connection", (socket) => {
+    // console.log(`User connected ${socket.id}`);
     let current_room = 0
-    console.log(`User connected ${socket.id}`);
     socket.on('room_number', (room) => {
         socket.join(room.room)
         current_room = room.room
+        io.to(current_room).emit('user_list', Object.values(users))
     })
-    
+
+    socket.on("joined_username", (data) => {
+        users[socket.id] = data
+        console.log(users);
+        io.to(current_room).emit('user_list', Object.values(users))
+    })
+
     socket.on('send_message', (data) => {
-        socket.broadcast.to(current_room).emit("receive_message", {data : data, sender : socket.id})
+        console.log(users);
+        socket.broadcast.to(current_room).emit("receive_message", { message: data.message, username: data.username })
     })
-})      
+
+    socket.on("disconnect", () => {
+        delete users[socket.id]
+        console.log(users);
+        io.to(current_room).emit('user_list', Object.values(users))
+    })
+})
+
 
 
 const port = process.env.PORT || 3001;
